571bb13503f8c10a67a9ed86e072074269c7505f
peterjbone <peterjbone41@gmail.com>
2024-03-17 11:29:53 -0500

    Favoriting system accomplish

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorites";
 
 interface HeartButtonProps {
 	listingId: string;
@@ -12,13 +13,15 @@ const HeartButton: React.FC<HeartButtonProps> = ({
 	listingId,
 	currentUser
 }) => {
-	const hasFavorites = false;
-	const toggleFavorites = () => {};
+	const { hasFavorited, toggleFavorite } = useFavorite({
+		listingId,
+		currentUser
+	});
 
 	//prettier-ignore
 	return (
     <div
-      onClick={toggleFavorites}
+      onClick={toggleFavorite}
       className="
         relative
         hover:opacity-80
@@ -37,7 +40,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
       <AiFillHeart
         size={24}
         className={
-          hasFavorites ? "fill-rose-500" : "fill-neutral-500/70"
+          hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"
         }
       />
     </div>
--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -112,7 +112,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
             $ {price}
           </div>
           {!reservation && (
-            <div className="font-light">night</div>
+            <div className="font-light">per night</div>
           )}
         </div>
         {onAction && actionLabel && (
