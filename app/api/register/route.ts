68682834e0f74c1879a28a0f77c8b2d0be723ec8
peterjbone <peterjbone41@gmail.com>
2024-04-21 22:27:24 -0500

    Fixing api/register

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,21 +9,16 @@ export async function POST(request: Request) {
 
 	const { email, name, password } = body;
 
-	try {
-		const hashedPassword = await bcrypt.hash(password, 12);
+	const hashedPassword = await bcrypt.hash(password, 12);
 
-		const user = await prisma.user.create({
-			data: {
-				email,
-				emailVerified: null,
-				name,
-				hashedPassword
-			}
-		});
+	const user = await prisma.user.create({
+		data: {
+			email,
+			emailVerified: null,
+			name,
+			hashedPassword
+		}
+	});
 
-		return NextResponse.json(user);
-	} catch (error) {
-		console.log(error);
-		return NextResponse.error();
-	}
+	return NextResponse.json(user);
 }
